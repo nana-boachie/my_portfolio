f4e1a4cfcedb6d2778fb7ed0489c482410ddcb16
Nana Kwame Yiadom Boachie <yiadom_deb@yiadom.net>
2024-03-04 09:27:29 +0000

    blog and portfolio

---
+++ yiadom_site/yiadom_blog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YiadomBlogConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "yiadom_blog"
---
+++ yiadom_site/yiadom_portfolio/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class YiadomPortfolioConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "yiadom_portfolio"
