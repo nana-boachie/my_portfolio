30ae3cbe77c4c8890fda3652259676e3fbae69b0
nana-boachie <160287824+nana-boachie@users.noreply.github.com>
2024-03-06 22:48:09 +0000

    I'm tired but added code to the views with one image, is Django the way?

--- yiadom_website/yiadom_website/settings.py
+++ yiadom_website/yiadom_website/settings.py
@@ -116,7 +116,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = 'static_files/'
+MEDIA_URL = 'images/'
+
+STATICFILES_DIRS = [
+    Path(BASE_DIR, 'static_files')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
